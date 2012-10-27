892acd30d3d86e34c55e69580621977edcface37
Michał Dettlaff <m.dettlaff@moje-biuro.pl>
2012-10-27 19:07:54 +0200

    add more comics

--- src/main/java/mdettlaff/comics/repository/ComicsRepository.java
+++ src/main/java/mdettlaff/comics/repository/ComicsRepository.java
@@ -16,7 +16,37 @@ public class ComicsRepository {
 		List<Comic> comics = new ArrayList<Comic>();
 		comics.add(new Comic("Dilbert", "http://www.dilbert.com/fast/", "^<img src=\"(.*?)\" />"));
 		comics.add(new Comic("xkcd", "http://xkcd.com/", "embedding\\): (.*?)$"));
+		comics.add(new Comic("Not Invented Here", "http://notinventedhe.re/", "<img alt=\"Not Invented Here.*? src=\"(.*?)\""));
+		comics.add(new Comic("Sinfest", "http://sinfest.net/", "<img src=\"(.*?comikaze.*?)\""));
+		comics.add(new Comic("Frefall", "http://freefall.purrsia.com/", "<img src=\"(.*?)\""));
+		comics.add(new Comic("Questionable Content", "http://questionablecontent.net/", "<img id=\"strip\" src=\"(.*?)\">$"));
+		comics.add(new Comic("PHD Comics", "http://phdcomics.com/comics.php", "<img.*?src=(.*?comics/archive/phd.*?) "));
+		comics.add(new Comic("Penny Arcade", "http://www.penny-arcade.com/comic/", "\\s+<img src=\"(http://art.*?)\""));
+		comics.add(new Comic("Cyanide & Happiness", "http://explosm.net/comics/", "<img.*? src=\"(.*?net/db/files/Comics.*?)\""));
+		comics.add(new Comic("SMBC", "http://www.smbc-comics.com/", "\\s+<img src=\'(.*?)\'><br>$"));
+		comics.add(new Comic("SMBC bonus", "http://www.smbc-comics.com/", "<img src=\'(.*?after.*?)\'>$"));
+		comics.add(new Comic("Abstruse Goose", "http://abstrusegoose.com/", "<img.*? src=\"(.*?strips.*?)\""));
+		comics.add(new Comic("Dinosaur Comics", "http://qwantz.com/index.php", "<img src=\"(.*?)\" class=\"comic\""));
+		comics.add(new Comic("User Friendly", "http://userfriendly.org/", "<IMG ALT=\"Latest Strip\".*? SRC=\"(.*?)\""));
+		comics.add(new Comic("Cowbirds in Love", "http://cowbirdsinlove.com/", "<img src=\"(.*?)\".*id=\"comic\""));
+		comics.add(new Comic("Chainsawsuit", "http://www.chainsawsuit.com", "<div id=\"comic.*?<img src=\"(.*?)\""));
+		comics.add(new Comic("Nedroid", "http://www.nedroid.com", "<img src=\"(.*?nedroid.com/comics.*?)\""));
+		comics.add(new Comic("FLOABC", "http://www.forlackofabettercomic.com", "<img src=\"(.*?img/comic.*?)\""));
+		comics.add(new Comic("Sequential Art", "http://www.collectedcurios.com/sequentialart.php", "<img id=\"strip\" src=\"(.*?)\""));
+		comics.add(createGoComicsComic("Garfield", "garfield"));
+		comics.add(createGoComicsComic("Monty", "monty"));
+		comics.add(createGoComicsComic("Pearls Before Swine", "pearlsbeforeswine"));
+		comics.add(createGoComicsComic("Scary Gary", "scarygary"));
+		comics.add(createGoComicsComic("Meaning of Lila", "meaningoflila"));
+		comics.add(createGoComicsComic("On a Claire Day", "onaclaireday"));
+		comics.add(createGoComicsComic("Daddy's Home", "daddyshome"));
+		comics.add(createGoComicsComic("Cafe con Leche", "cafeconleche"));
+		comics.add(createGoComicsComic("Calvin and Hobbes", "calvinandhobbes"));
 		return comics;
+	}
+
+	private Comic createGoComicsComic(String name, String urlPart) {
+		return new Comic(name, "http://gocomics.com/" + urlPart, "class=\"strip\".*?src=\"(.*?)\"");
 	}
 
 	public void clear() {
