fcb87e766129d33c06e18c4af0fcca013e414dd9
Michał Dettlaff <m.dettlaff@moje-biuro.pl>
2012-10-28 13:26:48 +0100

    remove unused imports

--- src/main/java/mdettlaff/comics/web/ComicsController.java
+++ src/main/java/mdettlaff/comics/web/ComicsController.java
@@ -12,9 +12,6 @@ import mdettlaff.comics.service.ComicsService;
 
 import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
