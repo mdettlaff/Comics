e9cba968b766d43faf472e4b2ca0b482a8853a3c
Michał Dettlaff <mdettlaff@jitsolutions.pl>
2015-02-08 18:12:17 +0100

    update Dilbert regex

--- src/main/java/mdettlaff/comics/repository/ComicsRepository.java
+++ src/main/java/mdettlaff/comics/repository/ComicsRepository.java
@@ -17,7 +17,7 @@ public class ComicsRepository {
 
 	public List<Comic> getAllKnownComics() {
 		List<Comic> comics = new ArrayList<Comic>();
-		comics.add(new Comic("Dilbert", "http://www.dilbert.com/fast/", "^<img src=\"(.*?)\" />"));
+		comics.add(new Comic("Dilbert", "http://www.dilbert.com", " img-comic\".*?src=\"(.*?)\""));
 		comics.add(new Comic("xkcd", "http://xkcd.com/", "embedding\\): (.*?)$"));
 		comics.add(new Comic("Not Invented Here", "http://notinventedhe.re/", "<img alt=\"Not Invented Here.*? src=\"(.*?)\""));
 		comics.add(new Comic("Sinfest", "http://sinfest.net/", "<img src=\"(.*?/comics/.*?)\""));
